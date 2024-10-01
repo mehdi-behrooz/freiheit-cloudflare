c63751eb78a4a4d36ef074e87b54dbe468ba7405
Mehdi <mehdi.behrooz@gmail.com>
2024-10-01 12:26:46 +0000

    fix linting errors

--- bash/process-zone-status.sh
+++ bash/process-zone-status.sh
@@ -6,7 +6,7 @@ ns2="$5"
 
 if [[ "$status" != "active" ]]; then
     echo "Zone is not active yet." \
-       "Make sure that you have set your host DNS to $ns1 and $ns2"
+        "Make sure that you have set your host DNS to $ns1 and $ns2"
     exit 1
 fi
 
